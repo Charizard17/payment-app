dd75a7265a58957807f2e3c14f31b791bf246cd9
esdu <3723esad@gmail.com>
2020-09-14 20:33:13 +0200

    success page created

--- charge.php
+++ charge.php
@@ -1,7 +1,7 @@
 <?php
     require_once('vendor/autoload.php');
 
-    $stripe = new \Stripe\StripeClient('sk_test_51HRGbCIv94jkZW9NkWmYVqIXbihGgBKq1locgedEuE0GzYegLB7gAKMkF4TtiEsNbCb3pXXwYs9ShZuLsVD42hpQ00BNuxpSRa');
+    \Stripe\Stripe::setApiKey('sk_test_51HRGbCIv94jkZW9NkWmYVqIXbihGgBKq1locgedEuE0GzYegLB7gAKMkF4TtiEsNbCb3pXXwYs9ShZuLsVD42hpQ00BNuxpSRa');
 
     // Sanitize POST Array
     $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
@@ -13,4 +13,19 @@
     $email = $POST['email'];
     $token = $POST['stripeToken'];
 
-    echo $token;
+    // Create Customer in Stripe
+    $customer = \Stripe\Customer::create(array(
+        "email" => $email,
+        "source" => $token
+    ));
+
+    // Charge Customer
+    $charge = \Stripe\Charge::create(array(
+        "amount" => 5000,
+        "currency" => "usd",
+        "description" => "Intro To React Course",
+        "customer" => $customer->id
+    ));
+
+    // Redirect to success
+    header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
---
+++ success.php
@@ -0,0 +1,28 @@
+<?php
+    if(!empty($_GET['tid'] && !empty($_GET['product']))) {
+        $GET = filter_var_array($_GET, FILTER_SANITIZE_STRING);
+
+        $tid = $GET['tid'];
+        $product = $GET['product'];
+    } else {
+        header('Location: index.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Thank You!</title>
+</head>
+<body>
+    <div class="container mt-4">
+        <h2>Thank you for purchasing <?php echo $product; ?></h2>
+        <hr>
+        <p>Your transaction ID is <?php echo $tid; ?></p>
+        <p>Check your email for more info</p>
+        <p><a href="index.php" class="btn btn-light mt-2">Go Back</a></p>
+    </div>
+</body>
+</html>
