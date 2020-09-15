ddaa65488d20a239b4fcda2415a9a07c71dc6a98
esdu <3723esad@gmail.com>
2020-09-15 11:50:42 +0200

    customers & transaction models created

--- charge.php
+++ charge.php
@@ -1,31 +1,64 @@
 <?php
-    require_once('vendor/autoload.php');
+  require_once('vendor/autoload.php');
+  require_once('config/db.php');
+  require_once('lib/pdo_db.php');
+  require_once('models/Customer.php');
+  require_once('models/Transaction.php');
 
-    \Stripe\Stripe::setApiKey('sk_test_51HRGbCIv94jkZW9NkWmYVqIXbihGgBKq1locgedEuE0GzYegLB7gAKMkF4TtiEsNbCb3pXXwYs9ShZuLsVD42hpQ00BNuxpSRa');
+  \Stripe\Stripe::setApiKey('sk_test_51HRGbCIv94jkZW9NkWmYVqIXbihGgBKq1locgedEuE0GzYegLB7gAKMkF4TtiEsNbCb3pXXwYs9ShZuLsVD42hpQ00BNuxpSRa');
 
-    // Sanitize POST Array
-    $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+ // Sanitize POST Array
+ $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
 
-    $first_name = $_POST;
+ $first_name = $POST['first_name'];
+ $last_name = $POST['last_name'];
+ $email = $POST['email'];
+ $token = $POST['stripeToken'];
 
-    $first_name = $POST['first_name'];
-    $last_name = $POST['last_name'];
-    $email = $POST['email'];
-    $token = $POST['stripeToken'];
+// Create Customer In Stripe
+$customer = \Stripe\Customer::create(array(
+  "email" => $email,
+  "source" => $token
+));
 
-    // Create Customer in Stripe
-    $customer = \Stripe\Customer::create(array(
-        "email" => $email,
-        "source" => $token
-    ));
+// Charge Customer
+$charge = \Stripe\Charge::create(array(
+  "amount" => 5000,
+  "currency" => "usd",
+  "description" => "Intro To React Course",
+  "customer" => $customer->id
+));
 
-    // Charge Customer
-    $charge = \Stripe\Charge::create(array(
-        "amount" => 5000,
-        "currency" => "usd",
-        "description" => "Intro To React Course",
-        "customer" => $customer->id
-    ));
+// Customer Data
+$customerData = [
+  'id' => $charge->customer,
+  'first_name' => $first_name,
+  'last_name' => $last_name,
+  'email' => $email
+];
 
-    // Redirect to success
-    header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
+// Instantiate Customer
+$customer = new Customer();
+
+// Add Customer To DB
+$customer->addCustomer($customerData);
+
+
+// Transaction Data
+$transactionData = [
+  'id' => $charge->id,
+  'customer_id' => $charge->customer,
+  'product' => $charge->description,
+  'amount' => $charge->amount,
+  'currency' => $charge->currency,
+  'status' => $charge->status,
+];
+
+// Instantiate Transaction
+$transaction = new Transaction();
+
+// Add Transaction To DB
+$transaction->addTransaction($transactionData);
+
+// Redirect to success
+header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
